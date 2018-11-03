7462256b70b7a86bcacf07c3d4516d42b19a1b0a
Joep Sijtsma <j.a.sijtsma@gmail.com>
2018-11-03 13:17:26 +0100

    WWW Control
    Added Shaders folder
    Added system for simple get and post request

---
+++ nFramework/Assets/Scripts/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nFrameWork.WWWControls;
+
+public class TestScript : MonoBehaviour {
+
+    WWWController _wwwController;
+
+    void Start()
+    {
+        WWWController.BaseUrl = "http://www.joepsijtsma.com/api/";
+
+        _wwwController = this.gameObject.AddComponent<WWWController>();
+        _wwwController.GetRequest("ballrollerlevels", "test");
+
+        StartCoroutine(WaitForResponse());
+    }
+
+    IEnumerator WaitForResponse()
+    {
+        string response = _wwwController.GetResponse("test");
+        while(response == null)
+        {
+            yield return new WaitForSeconds(1);
+            response = _wwwController.GetResponse("test");
+        }
+
+        Debug.Log(response);
+    }
+
+}
---
+++ nFramework/Assets/Scripts/WWW Control/WWWController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nFrameWork.WWWControls
+{
+    public class WWWController : MonoBehaviour
+    {
+        static string _baseUrl;
+        public static string BaseUrl { set { _baseUrl = value; } }
+
+        Dictionary<string, object> _responses = new Dictionary<string, object>();
+
+        public void GetRequest(string url, string key)
+        {
+            StartCoroutine(Get(_baseUrl + url, key));
+        }
+
+        IEnumerator Get(string url, string key)
+        {
+            WWW www = new WWW(url);
+            yield return www;
+            if (www.error != null)
+            {
+                Debug.LogError(url + " gives error: " + www.error);
+            }
+            else
+            {
+                _responses.Add(key, www.text);
+            }
+        }
+
+        public void PostRequest(string url, string key, string dataKey, string jsonData)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField(dataKey, jsonData);
+        }
+
+        IEnumerator Post(string url, WWWForm form)
+        {
+            WWW www = new WWW(url, form);
+            yield return www;
+            if(www.error != null)
+            {
+                Debug.LogError(url + " gives error: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Succesfully uploaded " + form.data + " to " + url);
+            }
+        }
+
+        public string GetResponse(string key)
+        {
+            object value;
+            if(_responses.TryGetValue(key, out value))
+            {
+                return (string)value;
+            }
+            return null;
+        }
+    }
+}
